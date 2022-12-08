1af1c9c0ee0155c8b0c24a16b2c216ea42319842
Casey Pan <casey871111@gmail.com>
2022-12-08 15:33:22 -0600

    topictitle changed

--- client/src/pages/Home/TopicBox.tsx
+++ client/src/pages/Home/TopicBox.tsx
@@ -174,11 +174,9 @@ const TopicBox = () => {
                 <Card>
                     {/* <Card.Img variant="top" src="holder.js/100px180" />  */}
                     <Card.Body>
-                        <Card.Title>Card Title {topicTitle}
+                        <Card.Title> {topicTitle}
                         </Card.Title>
                         <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
                         {topicContext}
                         </Card.Text>
                         <CloseIcon  onClick={handleClickSummaryButton}>Close</CloseIcon>
